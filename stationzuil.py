2a0110e305eb626dd08deadba9bfd1ac3de30f0e
Soulie-prog <144674259+Soulie-prog@users.noreply.github.com>
2023-09-25 11:24:22 +0200

    het bericht; de datum en tijd van het bericht; de naam van de reiziger – als de reiziger geen naam invult, gebruik dan als naam ‘anoniem’; het station – deze locatie van de zuil mag in de module zelf worden vastgelegd op basis van een random choice van drie stations. De computer (jouw

--- stationzuil.py
+++ stationzuil.py
@@ -2,26 +2,29 @@ import datetime
 import random
 import time
 import sys
-
+datum = datetime.datetime.now()
+print(datum)
 Tijd_Nu = datetime.datetime.now().hour
 
-if 1 <= Tijd_Nu < 5:
-    print('')
-    sys.exit()
+while True:
+    bericht = input('Vul hier uw bericht in alstublieft (Max 140 karakters):')
 
-bericht = input('Vul hier uw bericht in alstublieft (Max 140 karakters):')
+    if len(bericht) > 140:
+        print('Uw bericht is langer dan 140 karakters. Probeer opnieuw.')
+        continue  #  continue om opnieuw te vragen om een bericht
 
-if 1 <= len(bericht) <= 140:
+    if len(bericht) == 0:
+        print('Uw bericht is leeg. Probeer opnieuw.')
+        continue  # Gebruik continue om opnieuw te vragen om een bericht
+
     print('Uw bericht is opgeslagen!')
+    break
 
-elif len(bericht) > 140:
-    print('Uw bericht is langer dan 140 karakters')
-elif len(bericht) == 0:
-    print('u heeft niks ingevuld')
-    print('Progamma sluiten...')
-    time.sleep(2)
-    print( 'Programma gesloten.')
+if 1.00<= Tijd_Nu < 5.00:
+    print('Stationszuil kan op deze tijd niet worden gebruikt.')
+    print('Zuil wordt afgesloten...')
     sys.exit()
+
 
 confirmatie= input('weet u zeker dat u wil doorgaan? (ja/nee)')
 
@@ -42,13 +45,13 @@ if len(Naam) >= 1:
 else:
     print('U bent anoniem')
 
-Stations_lijst= ['alkmaar', 'Utrecht', 'Zeist', 'Maarn']
+with open('data.txt', 'r') as file:
+    lijst = file.readlines()
 
-Station_Keuze = random.choice(Stations_lijst)  # Kiest een random station uit lijst
-
+Station_Keuze = random.choice(lijst).strip(' \t\n')
 print(f'Uw bericht is vanaf Station {Station_Keuze} gemaakt.')
 
-Goedkeuring= input('weet u zeker dat u dit bericht wil verzenden?(ja/nee)')
+Goedkeuring = input('weet u zeker dat u dit bericht wil verzenden?(ja/nee)')
 
 if Goedkeuring == 'ja':
     print('Bericht is verzonden')
@@ -57,10 +60,12 @@ else:
     print('Bericht wordt verwijderd...')
     print('Bericht verwijderd!')
 
-
-if 6 <= Tijd_Nu < 18:
-    print(' Heb nog een prettige dag!')
-
+if 12.00 <= Tijd_Nu < 18.00:
+    print('Heb nog een prettige middag dag!')
+elif 6.00 <= Tijd_Nu < 11.59:
+    print('fijne dag!')
 else:
     print('Heb nog een fijne avond!')
+
+
 
